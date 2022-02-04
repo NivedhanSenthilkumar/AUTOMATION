dd26f497877491dfc454dceb76c95caa714c21f2
Nivedhan <nivedhan1998@gmail.com>
2022-02-04 16:19:56 +0530

    git commit ExcelAutomation.py

--- ExcelAutomation.py
+++ ExcelAutomation.py
@@ -1,22 +1,6 @@
 
 
 #1-Writing dataframes to Multiple sheets of the same workbook
-import pandas as pd
-# create  data_frame1 by creating a dictionary
-# in which values are stored as list
-data_frame1 = pd.DataFrame({'Fruits': ['Appple', 'Banana', 'Mango',
-                                       'Dragon Fruit', 'Musk melon', 'grapes'],
-                            'Sales in kg': [20, 30, 15, 10, 50, 40]})
-# create  data_frame2 by creating a dictionary
-# in which values are stored as list
-data_frame2 = pd.DataFrame({'Vegetables': ['tomato', 'Onion', 'ladies finger',
-                                           'beans', 'bedroot', 'carrot'],
-                            'Sales in kg': [200, 310, 115, 110, 55, 45]})
-# create  data_frame3 by creating a dictionary
-# in which values are stored as list
-data_frame3 = pd.DataFrame({'Baked Items': ['Cakes', 'biscuits', 'muffins',
-                                            'Rusk', 'puffs', 'cupcakes'],
-                            'Sales in kg': [120, 130, 159, 310, 150, 140]})
 # create a excel writer object
 with pd.ExcelWriter("D:/filename.xlsx") as writer:
     # use to_excel function and specify the sheet_name and index
